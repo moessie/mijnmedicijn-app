2ca41d6d526d62f951a48f977f869691f892d56a
Mustafa Aleshekir <moessieweb@gmail.com>
2021-01-24 20:01:19 +0100

    auth nav toegevoegd

--- app/src/main/java/com/example/mustafa/mijnmedicijn/Authentication/SignInFragment.java
+++ app/src/main/java/com/example/mustafa/mijnmedicijn/Authentication/SignInFragment.java
@@ -3,10 +3,12 @@ package com.example.mustafa.mijnmedicijn.Authentication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mustafa.mijnmedicijn.R;
 
@@ -16,6 +18,11 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        TextView SignUpSwitchTV = view.findViewById(R.id.SignUpSwitchTV);
+
+        SignUpSwitchTV.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_signInFragment_to_signUpFragment));
+
+        return view;
     }
 }
--- app/src/main/java/com/example/mustafa/mijnmedicijn/Authentication/SignUpFragment.java
+++ app/src/main/java/com/example/mustafa/mijnmedicijn/Authentication/SignUpFragment.java
@@ -3,10 +3,12 @@ package com.example.mustafa.mijnmedicijn.Authentication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mustafa.mijnmedicijn.R;
 
@@ -16,6 +18,13 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        final View view =  inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        TextView SignInSwitchTV = view.findViewById(R.id.SignInSwitchTV);
+
+        SignInSwitchTV.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_signUpFragment_to_signInFragment));
+
+
+        return view;
     }
 }
