ee5006bd38a8d6d1b8fe85ca864cf4bcf25d5329
Mustafa Aleshekir <moessieweb@gmail.com>
2021-01-24 19:53:23 +0100

    Auth schermen toegevoegd

---
+++ app/src/main/java/com/example/mustafa/mijnmedicijn/Authentication/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.example.mustafa.mijnmedicijn.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mustafa.mijnmedicijn.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
