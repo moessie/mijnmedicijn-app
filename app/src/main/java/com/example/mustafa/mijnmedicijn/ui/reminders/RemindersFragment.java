00238c50604f430965673940892fa5d402c24f61
Mustafa Aleshekir <moessieweb@gmail.com>
2021-01-25 13:18:34 +0100

    Reminder Ui

--- app/src/main/java/com/example/mustafa/mijnmedicijn/ui/reminders/RemindersFragment.java
+++ app/src/main/java/com/example/mustafa/mijnmedicijn/ui/reminders/RemindersFragment.java
@@ -6,16 +6,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.mustafa.mijnmedicijn.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RemindersFragment extends Fragment {
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_reminders, container, false);
+        final View view = inflater.inflate(R.layout.fragment_reminders, container, false);
+        FloatingActionButton addReminderFAB = view.findViewById(R.id.addReminderFAB);
+        addReminderFAB.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_navigation_reminders_to_addReminderFragment));
 
-        return root;
+
+        return view;
     }
 }
